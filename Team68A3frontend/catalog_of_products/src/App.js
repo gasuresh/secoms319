5cbb5a5ef2fbafc06c39d38abfa917959ddb8f04
wernatt <iamwpe@gmail.com>
2023-04-30 20:34:50 -0500

    delete work

--- Team68A3frontend/catalog_of_products/src/App.js
+++ Team68A3frontend/catalog_of_products/src/App.js
@@ -12,6 +12,7 @@ function App() {
     setReadButton(false)
     setUpdateButton(false);
     setDeleteButton(false);
+    setAboutUsButton(false)
 
   };
 
@@ -21,12 +22,13 @@ function App() {
     setReadButton(true)
     setUpdateButton(false);
     setDeleteButton(false);
+    setAboutUsButton(false)
 
     fetch("http://localhost:4000/")
-    .then((response) => response.json())
-    .then((data) => {
-      setProducts(data);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      });
 
   };
 
@@ -36,6 +38,7 @@ function App() {
     setReadButton(false)
     setUpdateButton(true);
     setDeleteButton(false);
+    setAboutUsButton(false)
 
   };
 
@@ -45,6 +48,17 @@ function App() {
     setReadButton(false)
     setUpdateButton(false);
     setDeleteButton(true);
+    setAboutUsButton(false)
+
+  };
+
+  const [aboutUsButton, setAboutUsButton] = useState(false);
+  const handleAboutUs = () => {
+    setCreateButton(false);
+    setReadButton(false)
+    setUpdateButton(false);
+    setDeleteButton(false);
+    setAboutUsButton(true)
 
   };
 
@@ -56,17 +70,17 @@ function App() {
       <Container>
         <h1 className="display-3 text-center">Catalog of Products</h1>
       </Container>
-      <CRUDOptions handleCreateButton= {handleCreateButton} handleReadButton = {handleReadButton} handleUpdateButton = {handleUpdateButton} handleDeleteButton = {handleDeleteButton}/>
-      {createButton && <ProductForm />}
+      <CRUDOptions handleCreateButton={handleCreateButton} handleReadButton={handleReadButton} handleUpdateButton={handleUpdateButton} handleDeleteButton={handleDeleteButton} />
+      {createButton && <ProductForm createButton={createButton} deleteButton={deleteButton}/>}
       {readButton && <ProductsList products={products} />}
-      {updateButton && <ProductForm />}
-      {deleteButton && <DeleteForm />}
+      {updateButton && <ProductForm createButton={createButton} deleteButton={deleteButton}/>}
+      {deleteButton && <ProductForm createButton={createButton} deleteButton={deleteButton}/>}
 
     </>
   );
 }
 
-const ProductForm = (/*{formData, handleFormSubmission, handleInputChange}*/) => {
+const ProductForm = ({createButton, deleteButton}) => {
 
   const [formData, setFormData] = useState({
     _id: "",
@@ -78,6 +92,16 @@ const ProductForm = (/*{formData, handleFormSubmission, handleInputChange}*/) =>
     rating: { rate: "", count: "" },
   });
 
+  const handleFind = () => {
+      const fetchProduct = async () => {
+        const response = await fetch(`http://localhost:4000/${formData._id}`);
+        const product = await response.json();
+        console.log(product)
+        setFormData(product);
+      };
+      fetchProduct();
+
+  }
 
 
 
@@ -100,10 +124,30 @@ const ProductForm = (/*{formData, handleFormSubmission, handleInputChange}*/) =>
         }
       });
   }
-  
+
+  const handleSubmitUpdate = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    fetch(`http://localhost:4000/update/${formData._id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Updating the product completed");
+        console.log(data);
+        if (data) {
+          //const keys = Object.keys(data);
+          const value = Object.values(data);
+          alert(value);
+        }
+      });
+  }
+
 
   const handleInputChange = (event) => {
-    
+
     const { name, value } = event.target;
     if (name === "count") {
       setFormData((formData) => ({
@@ -122,103 +166,116 @@ const ProductForm = (/*{formData, handleFormSubmission, handleInputChange}*/) =>
       }));
     }
   };
+   
+  const handleSubmitDelete = (e) => {
 
+  }
 
   return (
     <Container className='bg-light mx-auto my-5'>
-      <h1 className="display-6">Add New Product</h1>
-      <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="_id">
-        <Form.Label>Item Id</Form.Label>
-        <Form.Control
-          type="text"
-          name="_id"
-          value={formData._id}
-          onChange={handleInputChange}
-        />
-      </Form.Group>
+  {createButton && <h1 className="display-6">Add New Product</h1>}
+  {deleteButton && <h1 className="display-6">Delete Product</h1>}
+  {!createButton && !deleteButton && <h1 className="display-6">Update Product</h1>}
+  <Form onSubmit={createButton ? handleSubmit : deleteButton ? handleSubmitDelete : handleSubmitUpdate}>
 
-      <Form.Group controlId="title">
-        <Form.Label>Title</Form.Label>
-        <Form.Control
-          type="text"
-          name="title"
-          value={formData.title}
-          onChange={handleInputChange}
-        />
-      </Form.Group>
+    <Form.Group controlId="_id">
+      <Form.Label>Item Id</Form.Label>
+      <Form.Control
+        type="text"
+        name="_id"
+        value={formData._id}
+        onChange={handleInputChange}
+      />
+      {!createButton && <Button variant="outline-secondary" onClick={handleFind}>Find</Button>}
+    </Form.Group>
 
-      <Form.Group controlId="price">
-        <Form.Label>Price</Form.Label>
-        <Form.Control
-          type="text"
-          name="price"
-          value={formData.price}
-          onChange={handleInputChange}
-        />
-      </Form.Group>
+    <Form.Group controlId="title">
+      <Form.Label>Title</Form.Label>
+      <Form.Control
+        type="text"
+        name="title"
+        value={formData.title}
+        onChange={handleInputChange}
+        disabled={!createButton} // Disable the field if createButton is false
+      />
+    </Form.Group>
 
-      <Form.Group controlId="description">
-        <Form.Label>Description</Form.Label>
-        <Form.Control
-          type="text"
-          name="description"
-          value={formData.description}
-          onChange={handleInputChange}
-        />
-      </Form.Group>
+    <Form.Group controlId="price">
+      <Form.Label>Price</Form.Label>
+      <Form.Control
+        type="text"
+        name="price"
+        value={formData.price}
+        onChange={handleInputChange}
+        disabled={deleteButton} // Always enable the price field
+      />
+    </Form.Group>
 
-      <Form.Group controlId="category">
-        <Form.Label>Category</Form.Label>
-        <Form.Control
-          type="text"
-          name="category"
-          value={formData.category}
-          onChange={handleInputChange}
-        />
-      </Form.Group>
+    <Form.Group controlId="description">
+      <Form.Label>Description</Form.Label>
+      <Form.Control
+        type="text"
+        name="description"
+        value={formData.description}
+        onChange={handleInputChange}
+        disabled={!createButton} // Disable the field if createButton is false
+      />
+    </Form.Group>
 
-      <Form.Group controlId="image">
-        <Form.Label>Image</Form.Label>
-        <Form.Control
-          type="text"
-          name="image"
-          value={formData.image}
-          onChange={handleInputChange}
-        />
-      </Form.Group>
+    <Form.Group controlId="category">
+      <Form.Label>Category</Form.Label>
+      <Form.Control
+        type="text"
+        name="category"
+        value={formData.category}
+        onChange={handleInputChange}
+        disabled={!createButton} // Disable the field if createButton is false
+      />
+    </Form.Group>
 
-      <Form.Group controlId="rate">
-        <Form.Label>Rating</Form.Label>
-        <Form.Control
-          type="text"
-          name="rate"
-          value={formData.rating.rate}
-          onChange={handleInputChange}
-        />
-      </Form.Group>
+    <Form.Group controlId="image">
+      <Form.Label>Image</Form.Label>
+      <Form.Control
+        type="text"
+        name="image"
+        value={formData.image}
+        onChange={handleInputChange}
+        disabled={!createButton} // Disable the field if createButton is false
+      />
+    </Form.Group>
 
-      <Form.Group controlId="count">
-        <Form.Label># of Ratings</Form.Label>
-        <Form.Control
-          type="text"
-          name="count"
-          value={formData.rating.count}
-          onChange={handleInputChange}
-        />
-      </Form.Group>
+    <Form.Group controlId="rate">
+      <Form.Label>Rating</Form.Label>
+      <Form.Control
+        type="text"
+        name="rate"
+        value={formData.rating.rate}
+        onChange={handleInputChange}
+        disabled={!createButton} // Disable the field if createButton is false
+      />
+    </Form.Group>
 
-      <Button type="submit">Submit</Button>
-    </Form>
+    <Form.Group controlId="count">
+      <Form.Label># of Ratings</Form.Label>
+      <Form.Control
+        type="text"
+        name="count"
+        value={formData.rating.count}
+        onChange={handleInputChange}
+        disabled={!createButton} // Disable the field if createButton is false
+      />
+    </Form.Group>
 
+    <Button type="submit">{deleteButton ? "Delete" : "Submit"}</Button>
+  </Form>
+</Container>
 
-    </Container>
 
   );
 }
 
 const Product = (product) => {
-  const {_id, title, price, description, image, category, rating } = product.product;
+  const { _id, title, price, description, image, category, rating } = product.product;
   console.log(product)
 
   return (
@@ -242,7 +299,7 @@ const Product = (product) => {
 
 
 const ProductsList = ({ products }) => {
-  {console.log(products)}
+  { console.log(products) }
   return (
     <Container>
       <Row xs={1} sm={2} md={3} lg={4}>
@@ -256,41 +313,10 @@ const ProductsList = ({ products }) => {
   );
 };
 
-const DeleteForm = () => {
-  const [checked4, setChecked4] = useState(false);
-  const [index, setIndex] = useState(0);
-
-  const handleChange = (event) => {
-    setIndex(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(index);
-    // Do something with the index value, e.g. pass it to a parent component
-  };
-
-  return (
-    <Container className='bg-light mx-auto my-5'>
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formIndex">
-        <Form.Label>Enter an Index:</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter index"
-          value={index}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <button type="submit">Submit</button>
-    </Form>
-    </Container>
-  );
-};
 
 
 
-const CRUDOptions = ({handleCreateButton, handleReadButton, handleUpdateButton, handleDeleteButton}) => {
+const CRUDOptions = ({ handleCreateButton, handleReadButton, handleUpdateButton, handleDeleteButton }) => {
   return (
     <Container className='bg-dark mx-auto my-4 w-75'>
       <ButtonGroup className="d-flex justify-content-center">
@@ -298,6 +324,7 @@ const CRUDOptions = ({handleCreateButton, handleReadButton, handleUpdateButton, 
         <Button className="btn-block mx-1" variant="secondary" onClick={handleReadButton}>Read</Button>
         <Button className="btn-block mx-1" variant="info" onClick={handleUpdateButton}>Update</Button>
         <Button className="btn-block mx-1" variant="danger" onClick={handleDeleteButton}>Delete</Button>
+        <Button className="btn-block mx-1" variant="danger" onClick={handleDeleteButton}>About Us</Button>
       </ButtonGroup>
     </Container>
 
